4b4c983d9fa367fefe78424956816144d7ff30e6
iamitsinghchouhan <insidiouzzz@gmail.com>
2023-01-17 22:55:10 +0530

    Added Craco,Tailwind,yarn and required files

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge:["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode:false,
+  
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
